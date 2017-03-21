488c3a5b35865f88c878c6e6ef70a47feb2dcca2
kiranmayi <kiranmayi.donthineni@gmail.com>
2017-03-20 18:10:55 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/**
+ * Created by kavuri on 3/16/2017.
+ */
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //configure jshint to validate js files
+        jshint:{
+            options:{
+                reporter:require('jshint-stylish')
+            },
+            all :['Gruntfile.js','src/**/*.js']
+        },
+        //configure uglify to minify js files
+        uglify: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+            },
+            build: {
+                files: {
+                    'dist/js/magic.min.js': 'src/js/magic.js'
+                }
+            }
+        },
+        //configure  less stylesheets to css
+        less: {
+            build: {
+                files: {
+                    'dist/css/pretty.css': 'src/css/pretty.less'
+                }
+            }
+        },
+        //configure cssmin to minify css files
+        cssmin: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+            },
+            build: {
+                files: {
+                    'dist/css/style.min.css': 'src/css/style.css'
+                }
+            }
+        },
+        //configure watch to auto update
+        watch: {
+            stylesheets: {
+                files: ['src/**/*.css', 'src/**/*.less'],
+                tasks: ['less', 'cssmin']
+            },
+            scripts: {
+                files: 'src/**/*.js',
+                tasks: ['jshint', 'uglify']
+            }
+        }
+    });
+        //load Grunt PLUGINS
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+         grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+//create Tasks
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'less']);
+};
---
+++ src/js/magic.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kavuri on 3/16/2017.
+ */
+var hello = 'look im grunting';
+var awesome = 'yes it is awesome';
+var name = 'something';
+var friend = 'someone';
+var friends = 'hi all';
+var school = 'faria';
+var bhgf ='bhgf';
+var hi = 'hello';
+var hjnjknkd = 'hsuhdsjadjis';
+var ndnkdgmjkdfjgkfb = 'bjchdsjkfkdsj';
